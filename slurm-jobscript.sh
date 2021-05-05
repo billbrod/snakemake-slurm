b0bd73726ae3cfe3f6dc460c423a321369ccde40
Billy Broderick <wfb229@log-1.nyu.cluster>
2021-05-05 17:55:33 -0400

    removes unnecessary lines from jobscript

--- slurm-jobscript.sh
+++ slurm-jobscript.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 #SBATCH --export=NONE
-# properties = {properties}
-# . /share/apps/anaconda3/5.3.1/etc/profile.d/conda.sh
-# conda activate sfp
 env > /scratch/$USER/overlay/env.log
 
 if [ "$SINGULARITY_CONTAINER" == "" ]; then
