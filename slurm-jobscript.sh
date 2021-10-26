97caa797132f83e4321a4c22e57716af3d1d4a71
William F. Broderick <billbrod@gmail.com>
2021-10-26 15:37:35 -0400

    removes unnecessary echo

--- slurm-jobscript.sh
+++ slurm-jobscript.sh
@@ -2,9 +2,6 @@
 #SBATCH --export=SINGULARITY_CONTAINER_PATH,SFP_PATH
 # properties = {properties}
 
-echo $SINGULARITY_CONTAINER_PATH
-echo $SFP_PATH
-
 # q is a special formatting symbol used by snakemake to tell it to escape quotes
 # correctly
 $SFP_PATH/run_singularity.py $SINGULARITY_CONTAINER_PATH {exec_job:q}
